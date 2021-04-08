b9c2742c06fedce9eb8e8b01e07de108071afbf7
yerkebulan19971212 <yerkebulan19971212@gmail.com>
2021-04-08 18:55:34 +0600

    addd update product

--- store/urls.py
+++ store/urls.py
@@ -12,6 +12,17 @@ urlpatterns = [
     path('blog', blog, name="blog"),
     path('order', orders, name="order"),
     path('order/detail/<int:id>', detail),
+    # products
     path('add-product/', admin_add_product, name='add_product'),
+    path('products/<int:pk>', update_product, name='update_product'),
     path('products/', admin_products, name='admin_products'),
+    # blog
+    path('blog-list/', admin_blog_list, name='admin_blog_list'),
+    path('add-blog/', add_blog_admin, name='add_blog_admin'),
+    path('update-blog/<int:pk>', update_blog_admin, name='update_blog_admin'),
+    path('delete-blog-admin/<int:pk>', delete_blog_admin, name='delete_blog_admin'),
+    # orders
+    path('order-list/', order_list_admin, name='order_list_admin'),
+    path('order-detail-list/<int:pk>', order_detail_list_admin, name='order_detail_list'),
+
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from cart.cart import Cart
@@ -192,7 +192,8 @@ def admin_add_product(request):
         context = {
             'categories': categories,
             'statuses': statuses,
-            "segment": "products"
+            "segment": "products",
+            'action': 'Добавить продукт'
         }
         return render(request, 'admin/add-product.html', context)
 
@@ -201,3 +202,104 @@ def admin_add_product(request):
 def admin_products(request):
     list = Product.objects.all().order_by('-pk')
     return render(request, 'admin/products.html', {'list': list, "segment": "products"})
+
+
+def update_product(request, pk):
+    if request.method == "GET":
+        categories = Category.objects.all()
+        statuses = Status.objects.all()
+        product = Product.objects.get(pk=pk)
+        return render(request, 'admin/add-product.html', {"segment": "products", 'product': product, 'categories': categories,
+            'statuses': statuses, 'action': 'Обновить продукт'})
+
+    if request.method == "POST":
+        if request.method == "POST":
+            data = request.POST
+            name = data.get('name')
+            price = data.get('price')
+            description = data.get('description')
+            category = data.get('category')
+            status = data.get('status')
+            files = request.FILES
+            img1 = files.get('img1')
+            img2 = files.get('img2')
+            img3 = files.get('img3')
+            img4 = files.get('img4')
+            img5 = files.get('img5')
+
+            product = Product.objects.get(pk=pk)
+            product.name = name
+            product.price = price
+            product.description = description
+            product.category_id = category
+            product.status_id = status
+            if img5:product.img5 = img5
+            if img4:product.img4 = img4
+            if img3:product.img3 = img3
+            if img2:product.img2 = img2
+            if img1:product.img1 = img1
+            product.save()
+
+            return redirect('admin_products')
+
+def admin_blog_list(request):
+    list = Blog.objects.all().order_by('-pk')
+    return render(request, 'admin/blogs.html', {'list': list, "segment": "blog"})
+
+
+def add_blog_admin(request):
+    if request.method == "GET":
+        return render(request, 'admin/add-blog.html', {"segment": "blog", 'action': 'Добавить'})
+
+    if request.method == "POST":
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        img = request.FILES.get('img')
+
+        blog = Blog.objects.create(name=name, img=img, description=description)
+
+        return redirect('admin_blog_list')
+
+
+def update_blog_admin(request, pk):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        img = request.FILES.get('img')
+        print(img)
+        blog = Blog.objects.get(pk=pk)
+        blog.name = name
+        blog.description = description
+        if img:
+            blog.img = img
+        blog.save()
+
+        return redirect('admin_blog_list')
+
+    if request.method == "GET":
+        blog = Blog.objects.get(pk=pk)
+        name = blog.name
+        description = blog.description
+        img = blog.img
+        return render(request, 'admin/add-blog.html', {'name': name, 'description': description, 'img': img,
+                                                       'segment': 'blog', 'action': 'Обновить'})
+
+
+def delete_blog_admin(request, pk):
+    try:
+        del_sel = Blog.objects.get(pk=pk)
+    except id.DoesNotExist:
+        return redirect('admin_blog_list')
+    del_sel.delete()
+    return redirect('admin_blog_list')
+
+
+def order_list_admin(request):
+    list = Order.objects.all()
+    return render(request, 'admin/Orders.html', {'list': list, "segment": "order"})
+
+
+def order_detail_list_admin(request, pk):
+    order = Order.objects.get(pk=pk)
+    list = OrderDetail.objects.filter(order=order)
+    return render(request, 'admin/order-details.html', {'order': order, 'list': list, "segment": "order"})
